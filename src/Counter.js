129242774bc04447710c69e3778bb54c6d8aae9e
王兆帅 <g1king@163.com>
2020-07-04 11:14:18 +0800

    Revert "Merge branch 'master' of https://github.com/G1King/CNode- into dev"
    This reverts commit 4fd154db00f520916af6c9884517800e62b37d10, reversing
    changes made to 7a145246741421a3a1cd15a3eca5c5dc83eccbc2.

--- src/Counter.js
+++ src/Counter.js
@@ -7,7 +7,6 @@ export default class Counter extends Component {
 
   render() {
       const {value,onIncreasClick} = this.props;
-       console.log('222')
     return (
       <div>
         <span>{value}</span>
